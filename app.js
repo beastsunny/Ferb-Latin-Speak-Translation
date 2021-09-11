b4b42cdb8e3510f42adc5155d2ea096f25598929
beastsunny <beastsunny06@gmail.com>
2021-09-11 14:14:20 +0530

    style updated

--- app.js
+++ app.js
@@ -10,15 +10,17 @@ function getTranslationUrl(text) {
 }
 
 
+
 function errorHandler(error) {
     console.log(error);
     alert("Something is wrong with the server! Please try after sometime");
 }
 
-
 function clickHandlerOfTranslate() {
     var inputValue = txtInput.value;
     fetch(getTranslationUrl(inputValue)).then(Response => Response.json()).then(json => divOutput.textContent = json.contents.translated).catch(errorHandler);
 }
+
+
 
 btnTranslate.addEventListener("click", clickHandlerOfTranslate);
