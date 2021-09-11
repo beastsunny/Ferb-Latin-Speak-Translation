8a17db8e0d3baab2e4d86323011fa78c9d220886
beastsunny <beastsunny06@gmail.com>
2021-09-11 13:14:42 +0530

    feat: api server and click handler added

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 var txtInput = document.querySelector("#txtInput");
 var btnTranslate = document.querySelector("#btnTranslate");
 var divOutput = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+
+function getTranslationUrl(text) {
+    return serverUrl + "?text=" + text;
+}
+
+
+
+
+
+
+
+
+
+btnTranslate.addEventListener("click", clickHandlerofTranslate);
